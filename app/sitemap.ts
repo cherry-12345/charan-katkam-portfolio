f1708a067e3409d4850905e0c30526485af1fa56
Charan Katkam <130783197+cherry-12345@users.noreply.github.com>
2026-06-08 16:59:43 +0530

    Fix double slashes in sitemap by removing trailing slash from siteUrl

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from 'next'
 import { insightPosts } from './data/insightsPosts'
 
-const siteUrl = 'https://www.charankatkam.me/'
+const siteUrl = 'https://www.charankatkam.me'
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const now = new Date()
